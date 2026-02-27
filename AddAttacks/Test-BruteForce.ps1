a01135c5cf5e29fa3371f8400c0f398382e4a869
Bob Lyons <bob.lyons.202@gmail.com>
2026-02-27 10:15:02 -0500

    fix test brute force

--- AddAttacks/Test-BruteForce.ps1
+++ AddAttacks/Test-BruteForce.ps1
@@ -13,15 +13,31 @@ Write-Host "Starting $attempts bad-password attempts against '$netbios\$targetUs
 Write-Host "(using net use to force NTLM -> 4625 events)"
 Write-Host ""
 
+# clean up any existing connection first
+net use \\$dcName\netlogon /delete > $null 2>&1
+
 1..$attempts | ForEach-Object {
-    Write-Host "  attempt $_ "  -NoNewline
+    Write-Host "  attempt $_ " -NoNewline
     net use \\$dcName\netlogon /user:"$netbios\$targetUser" "WrongPassword!$_" > $null 2>&1
-    net use \\$dcName\netlogon /delete > $null 2>&1
     Write-Host "done"
 }
+
+# clean up
+net use \\$dcName\netlogon /delete > $null 2>&1
 
 Write-Host ""
 Write-Host "Done. Checking AD status..."
 Get-ADUser -Identity $targetUser -Properties LockedOut, BadLogonCount, LastBadPasswordAttempt |
     Select-Object SamAccountName, LockedOut, BadLogonCount, LastBadPasswordAttempt |
+    Format-List
+
+Write-Host "Checking Security log for 4625 events targeting $targetUser (last 2 minutes)..."
+$since = (Get-Date).AddMinutes(-2)
+Get-WinEvent -ComputerName $dcName -FilterHashtable @{
+    LogName   = 'Security'
+    Id        = 4625
+    StartTime = $since
+} -ErrorAction SilentlyContinue |
+    Where-Object { $_.Message -match $targetUser } |
+    Select-Object TimeCreated, Message |
     Format-List
